7af8989798288c8f5c26803f3d048fce0949cb71
James W. Kimani <james@eoncollective.com>
2021-07-23 21:34:27 +0000

    Modularity and the ref functions - staging

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -2,28 +2,20 @@
     materialized="table"
 )}}
 
+--- modularize by refering to stg_customer model as source
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+    select * from {{ ref('stg_customers')}}
 
 ),
 
+--- modularize by refering to stg_order models as source
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
+
 
 customer_orders as (
 
