07a3deb8fb10e84e00c35121f2c73b9b5237af15
James Kimani <james@eoncollective.com>
2022-09-19 22:56:42 -0400

    Added sources

--- models/staging/covid_epidemiology_keys.sql
+++ models/staging/covid_epidemiology_keys.sql
@@ -8,7 +8,7 @@
 }}
 
 with flatten_airbyte_data as (
-    select json_extract_path_text(json_serialize(_airbyte_data), 'key') from {{ var('env') }}.williams_dev._airbyte_raw_covid_epidemiology
+    select json_extract_path_text(json_serialize(_airbyte_data), 'key') from {{ 'covid','_airbyte_raw_covid_epidemiology'  }}
 ),
 final_cte as (
     select * from flatten_airbyte_data
