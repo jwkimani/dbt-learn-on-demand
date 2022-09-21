5a415eb710ec2876fb800f79a5138232d28ff0a0
James Kimani <james@eoncollective.com>
2022-09-20 23:09:19 -0400

    Fixed dataid hskey and bkey

--- models/raw_stage/vw_shelter.sql
+++ models/raw_stage/vw_shelter.sql
@@ -1,15 +1,16 @@
 {{
     config(
+        enabled=false,
         materialized="view",
         tags=["datavault", "dv", "juvare", "demo"],
         schema="dbo",
-        alias="dbt_table_1",
+        alias="dbt_shelters",
     )
 }}
 
 with source_cte as (
     select * from (
-        select case when prevdataid = 0 then dataid else prevdataid end shelter_bkey, a.* from {{ source('juvare','table_1')  } a}
+        select case when prevdataid = 0 then dataid else prevdataid end shelter_bkey, a.* from {{ source('juvare','shelters')  } a}
         )
 ),
 final_cte as (
